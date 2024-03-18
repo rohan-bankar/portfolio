24244372dfe90af7175c2ff4806fcba615419964
rohan-bankar <rohanbankar011@gmail.com>
2024-03-18 18:24:59 +0530

    updated

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -5,9 +5,9 @@ function Card(props) {
   return (
     <>
     <div className='lg:h-[200px] lg:w-[400px] rounded-md border-solid border-gray-300 hover:shadow-md hover:shadow-sky-500 border p-2 text-center max-sm:mb-5 sm:mx-2 '>
-        <h1 className={`text-2xl font-bold ${themeMode === 'light' ? 'text-black' : 'text-white'}`}>{props.title}</h1>
+        <h1 className={`text-xl font-bold ${themeMode === 'light' ? 'text-black' : 'text-white'}`}>{props.title}</h1>
         <hr />
-        <p className={`text-xl mt-5 font-thin ${themeMode === 'light' ? 'text-black' : 'text-white'}`}>{props.description}</p>
+        <p className={`text-lg mt-5 font-thin ${themeMode === 'light' ? 'text-black' : 'text-white'}`}>{props.description}</p>
         <hr className='my-2' />
         <a href={props.link} className='text-sky-500 font-bold hover:underline hover:text-sky-600' >github link</a>
     </div>
